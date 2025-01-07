2c33d8b3ef9e4953c791481c6744db1a8b87442e
Rajveer188 <veerkajle188@gmail.com>
2025-01-08 00:50:04 +0530

    update VolumeOfCylinder file

--- VolumeOfCylinder.java
+++ VolumeOfCylinder.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-class VolumeOfCircle{
+class VolumeOfCylinder{
    public static void main(String args[]){
      Scanner scan = new Scanner(System.in);
      System.out.println("Enter the radius : ");
      double radius = scan.nextDouble();
      System.out.println("ENTER THE HEIGHT : ");
      double height = scan.nextDouble();
-     double Volume = 3.14*radius*radius*height;
+     double volume = 3.14*radius*radius*height;
      System.out.println("Volume of cylinder : "+volume);
      scan.close();
    }
