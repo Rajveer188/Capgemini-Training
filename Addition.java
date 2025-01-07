2e87d127550e618b9df307df204d9039e5f0df89
Rajveer188 <veerkajle188@gmail.com>
2025-01-08 00:35:56 +0530

    update Addition File

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition{
+    public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+       System.out.println("Enter first number");
+       int num1 = scan.nextInt();
+       System.out.println("Enter second number");
+       int num2 = scan.nextInt();
+       
+       int result = num1+num2;
+       System.out.println("Result is "+ result);
+    }
+}
+
