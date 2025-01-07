afd7ee31cd4e46648043ff94d0cf931dac1eb4b5
Rajveer188 <veerkajle188@gmail.com>
2025-01-07 22:36:24 +0530

    add area of circle file

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class AreaOfcircle{
+  public static void main(){
+   Scanner scan = new Scanner(System.in);
+   System.out.println("enter the radius");
+   double radius = scan.nextDouble();
+   double circle = 3.14 * radius * radius;
+   System.out.println("area of circle : "+circle);
+   Scanner.close();
+  }
+}
